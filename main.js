566b1554c05a3a61b5589c1f885c19b131eaae47
Manan837 <89606852+Manan837@users.noreply.github.com>
2023-07-05 13:26:21 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -10,11 +10,28 @@ function setup()
     poseNet.on('pose', gotPoses);
 }
 
+function draw()
+{
+    background("#5196e3");
+    document.getElementById("font_size").innerHTML = "Font Size Of The Text Will Be = "+difference+"px";
+    textSize(difference);
+    fill("#00ff0a");
+    text('Manan',50,400);
+}
+
 function gotPoses(results)
 {
     if (results.length > 0)
     {
         console.log(results);
+    }
+    if(results.length > 0)
+    {
+        console.log(results);
+
+        leftWrist_x = results[0].pose.leftWrist.x;
+        rightWrist_x = results[0].pose.rightWrist.x;
+        difference = floor(leftWrist_x - rightWrist_x);
     }
 }
 
